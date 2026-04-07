6e406b8bea6d1a420fc286d390f920fb7fcd604c
Chahat Mittal <>
2026-04-07 06:36:48 +0000

    Fix storage account name

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,30 +1,18 @@
 terraform {
   required_version = ">= 1.6.0"
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.85"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.6"
-    }
   }
-
   backend "azurerm" {
     resource_group_name  = "banking-tfstate-rg"
-    storage_account_name = "bankingtfstate"
+    storage_account_name = "bankingtfstate321"
     container_name       = "tfstate"
     key                  = "banking.terraform.tfstate"
   }
 }
-
 provider "azurerm" {
-  features {
-    key_vault {
-      purge_soft_delete_on_destroy    = true
-      recover_soft_deleted_key_vaults = true
-    }
-  }
+  features {}
 }
